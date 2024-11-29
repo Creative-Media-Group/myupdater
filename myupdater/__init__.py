d840c5c06a0924ec1f3ee6e85622d147a505f466
tct123 <42028373+tct123@users.noreply.github.com>
2024-11-30 00:47:37 +0100

    Update __init__.py

--- myupdater/__init__.py
+++ myupdater/__init__.py
@@ -1,5 +1,10 @@
 import requests
 
 
-def updater(file_url):
-    pass
+def updater(file_url, version=""):
+    r = requests.get(url=file_url)
+    return r
+
+
+if __name__ == "__main__":
+    updater()
